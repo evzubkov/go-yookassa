27c5928627cf0911d1813f12533f6e752f8e6561
evzubkov <evzubkov@inbox.ru>
2024-05-23 12:28:25 +0700

    Добавил методы для двухстадийной оплаты

--- client.go
+++ client.go
@@ -30,16 +30,27 @@ type (
 		Type      string `json:"type"`
 		ReturnUrl string `json:"return_url"`
 	}
+	PaymentMethod struct {
+		Type string `json:"type"`
+	}
 	NewPaymentRequest struct {
-		Amount       Amount       `json:"amount"`
-		Capture      bool         `json:"capture"`
-		Confirmation Confirmation `json:"confirmation"`
-		Description  string       `json:"description"`
+		Amount            Amount        `json:"amount"`
+		Capture           bool          `json:"capture"`
+		Confirmation      Confirmation  `json:"confirmation"`
+		Description       string        `json:"description"`
+		PaymentMethod     PaymentMethod `json:"payment_method_data,omitempty"`
+		SavePaymentMethod string        `json:"save_payment_method,omitempty"`
 	}
 
 	NewPaymentResponse struct {
-		Id           string `json:"id"`
-		Status       string `json:"status"`
+		Id            string `json:"id"`
+		Amount        Amount `json:"amount"`
+		Status        string `json:"status"`
+		PaymentMethod struct {
+			Type  string `jsoon:"type"`
+			Id    string `json:"id"`
+			Saved bool   `json:"saved"`
+		} `json:"payment_method"`
 		Сonfirmation struct {
 			Type            string `json:"type"`
 			ConfirmationUrl string `json:"confirmation_url"`
@@ -120,6 +131,104 @@ func (o *Client) CheckPaymentStatus(ctx context.Context, paymentId string) (resu
 
 	req, _ := http.NewRequestWithContext(
 		ctx, "GET", fmt.Sprintf("https://api.yookassa.ru/v3/payments/%s", paymentId), nil)
+	req.SetBasicAuth(o.shopId, o.apiKey)
+	req.Header.Add("Idempotence-Key", uuid.New().String())
+	req.Header.Add("Content-Type", "application/json")
+
+	httpClient := http.Client{}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("fail to send response to server. Status code: %d, info: %+v", resp.StatusCode, cast.ToString(body))
+		return
+	}
+
+	if err = json.Unmarshal(body, &result); err != nil {
+		return
+	}
+
+	return
+}
+
+type CapturePaymentResponse struct {
+	Id            string `json:"id"`
+	Amount        Amount `json:"amount"`
+	Status        string `json:"status"`
+	PaymentMethod struct {
+		Type  string `jsoon:"type"`
+		Id    string `json:"id"`
+		Saved bool   `json:"saved"`
+	} `json:"payment_method"`
+}
+
+// CapturePayment  - move holding payment
+//
+// Transfer money with a two-stage character
+func (o *Client) CapturePayment(ctx context.Context, amount Amount, paymentId string) (result CapturePaymentResponse, err error) {
+
+	payload, err := json.Marshal(amount)
+	if err != nil {
+		return
+	}
+
+	req, _ := http.NewRequestWithContext(
+		ctx, "POST", fmt.Sprintf("https://api.yookassa.ru/v3/payments/%s/capture", paymentId), bytes.NewBuffer(payload))
+	req.SetBasicAuth(o.shopId, o.apiKey)
+	req.Header.Add("Idempotence-Key", uuid.New().String())
+	req.Header.Add("Content-Type", "application/json")
+
+	httpClient := http.Client{}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("fail to send response to server. Status code: %d, info: %+v", resp.StatusCode, cast.ToString(body))
+		return
+	}
+
+	if err = json.Unmarshal(body, &result); err != nil {
+		return
+	}
+
+	return
+}
+
+type CancelPaymentResponse struct {
+	Id            string `json:"id"`
+	Amount        Amount `json:"amount"`
+	Status        string `json:"status"`
+	Paid          bool   `json:"paid"`
+	PaymentMethod struct {
+		Type  string `jsoon:"type"`
+		Id    string `json:"id"`
+		Saved bool   `json:"saved"`
+	} `json:"payment_method"`
+}
+
+// CancelPayment  - cancel holding payment
+//
+// Cancel payment for two-step payment
+func (o *Client) CancelPayment(ctx context.Context, paymentId string) (result CancelPaymentResponse, err error) {
+
+	req, _ := http.NewRequestWithContext(
+		ctx, "POST", fmt.Sprintf("https://api.yookassa.ru/v3/payments/%s/cancel", paymentId), nil)
 	req.SetBasicAuth(o.shopId, o.apiKey)
 	req.Header.Add("Idempotence-Key", uuid.New().String())
 	req.Header.Add("Content-Type", "application/json")
